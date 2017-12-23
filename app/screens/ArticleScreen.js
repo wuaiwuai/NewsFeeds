584acb197194ff71f36b64f0f2007479580951ef
Jon Dang <jondang@gmail.com>
2017-12-23 16:08:30 -0500

    pass url to article page

--- app/screens/ArticleScreen.js
+++ app/screens/ArticleScreen.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-import { WebView } from 'react-native';
+import PropTypes from 'prop-types';
+import { Alert, WebView } from 'react-native';
 
 class ArticleScreen extends Component {
   render() {
+    const { params } = this.props.navigation.state;
     return (
       <WebView
-        source={{ uri: 'https://www.nytimes.com/interactive/2017/12/19/us/ford-chicago-sexual-harassment.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=span-ab-top-region&region=top-news&WT.nav=top-news' }}
+        source={{ uri: params.url }}
       />
     );
   }
 }
+
+ArticleScreen.propTypes = {
+  navigation: PropTypes.shape({
+    state: PropTypes.shape({
+      params: PropTypes.object,
+    }),
+  }),
+};
 
 export default ArticleScreen;
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -8,10 +8,12 @@ const sampleData = [
   {
     title: 'Apple Plans Combined iPhone, iPad & Mac Apps to Create One User Experience',
     description: 'Apple Inc.’s iPhone and iPad introduced a novel way of interacting with computers: via easy-to-use applications, accessible in the highly curated App Store.',
+    url: 'https://www.cnbc.com/2017/12/21/eric-schmidt-is-stepping-down-as-the-executive-chairman-of-alphabet.html',
   },
   {
     title: 'SpaceX’s Falcon Heavy rocket sets up at Cape Canaveral ahead of launch',
     description: 'SpaceX won\'t hit its 2017 target for a test flight of its new, huge Falcon Heavy rocket – but the massive launch craft is now in place at Cape Canaveral in..',
+    url: 'https://www.yahoo.com/',
   },
 ];
 
@@ -23,9 +25,8 @@ const styles = StyleSheet.create({
 });
 
 class HomeScreen extends React.Component {
-  onPressButton(navigate) {
-    navigate('Article');
-    // Alert.alert('You tapped the button!');
+  onPressButton(navigate, url) {
+    navigate('Article', { url });
   }
 
   render() {
@@ -41,7 +42,7 @@ class HomeScreen extends React.Component {
             <StoryListItem
               title={item.title}
               description={item.description}
-              onPressButton={() => this.onPressButton(navigate)} />
+              onPressButton={() => this.onPressButton(navigate, item.url)} />
           }
           keyExtractor={(item, index) => index}
         />
